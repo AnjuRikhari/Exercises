369b7f729e601382f4c3a24fd401a34c8f91a327
AnjuRikhari <kumarianju575@gmail.com>
2021-02-25 22:30:54 +0530

    Maven Exercise

---
+++ FirstMavenProject/src/main/java/com/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.mavenproject;
+
+public class App {
+    public void print()
+    {
+        System.out.println("Hello World ");
+    }
+    public static void main(String[] args)
+    {
+        App obj = new App();
+        obj.print();
+    }
+}
